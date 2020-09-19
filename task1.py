24d65b93a87ed81a8d4bafcb19f8137fd06d0257
parsidd <ee17b059@smail.iitm.ac.in>
2020-09-19 19:35:25 +0530

    Tried out task1 and generated graphs.

--- task1.py
+++ task1.py
@@ -13,19 +13,21 @@ from itertools import combinations
 backend = BasicAer.get_backend("statevector_simulator")
 
 N = 4
-MAX_DEPTH = 10
+MAX_DEPTH = 30
 params = []
 
 complete_circuit = QuantumCircuit(N)
-phi = random_statevector(2**N)
+phi = np.asarray(random_statevector(2**N).data)
 tol = 1e-3
+maxiter = 500
+final_vector = 0
 
 def even_block(theta):
     eb = QuantumCircuit(N)
-    for i in range(N):
-        eb.rz(theta[i], i)
     for i in combinations(range(N), 2):
         eb.cz(i[0], i[1])
+    for i in range(N):
+        eb.rz(theta[i], i)
     return eb
 
 def odd_block(theta):
@@ -39,10 +41,12 @@ def evaluate_cost(angles):
     for i, angle in enumerate(angles):
         params_dict[params[i]] = angle
     final_state = execute(complete_circuit.bind_parameters(params_dict), backend = backend).result().get_statevector()
-    print(np.linalg.norm((Statevector(final_state)-phi).data))
-    return np.linalg.norm((Statevector(final_state)-phi).data)
+    final_vector = final_state
+    temp = np.abs((final_vector-phi))
+    return temp.dot(temp)
 
-optimizer = COBYLA(maxiter=500, tol=tol)
+optimizer = COBYLA(maxiter=maxiter, tol=tol)
+costs = []
 
 for L in range(1, MAX_DEPTH+1):
     odd_params = [Parameter("theta_"+str(2*L-1)+"_"+str(i)) for i in range(N)]
@@ -54,11 +58,13 @@ for L in range(1, MAX_DEPTH+1):
     complete_circuit.append(even_block(even_params), range(N))
     complete_circuit.barrier()
     # print(complete_circuit.decompose().draw())
-    # print(len(params))
     results = optimizer.optimize(len(params), objective_function=evaluate_cost, initial_point=np.random.random(len(params)))
     # print("Phi = ", phi.data)
-    # print(results)
-    print("Number of layers = ", L, "Minimal cost = ",results[1], "Number of iterations = ", results[2])
+    print("\nNumber of layers = ", L, "\nMinimal cost = ",results[1], "\nNumber of iterations = ", results[2])#, "\nAngles = ", results[0])
+    costs.append(results[1])
 
-# angles = np.random.random(N)
-# print(np.linalg.norm(evaluate_cost(angles)))
+# angles = np.random.random(2*N)
+# x = evaluate_cost(angles)
+# print(np.sum())
+plt.plot(costs)
+plt.show()
